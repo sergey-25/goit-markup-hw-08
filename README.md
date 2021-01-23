fc5f7bd96f77002bf95d1dbc490f323fd85b390f
sergey kurylenko <sergey25111988@gmail.com>
2021-01-24 00:11:37 +0200

    menu/modal

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # goit-markup-hw-07
+<button class="menu-button" type="button" aria-expanded="false" aria-controls="menu-container" data-menu-button>
+           <svg  width="40" height="40" >
+                <use class="icon-menu" href="./images/header-modal.svg#icon-menu_40px"></use>
+                <use class="icon-cross" href="./images/header-modal.svg#icon-close_40px-1"></use>
+            </svg></nav>
+
+            </button>
--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,19 @@
-
-    
 (() => {
-  const refs = {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+
+  });
+})();
+/* (() => {const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
@@ -18,4 +30,4 @@
       bodyScrollLock[method](refs.menu);
     }
     console.log(bodyScrollLock);
-})();
+})();*/
